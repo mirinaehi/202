00d6d943bc45a9da15951b447f98cff2abad014a
MiJi <MiJi@School>
2022-04-20 09:16:55 +0900

    생성자 호출

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -9,9 +9,14 @@ private:
 	int nHakbun;
 	string sName;
 
+public:
 	// 생성자 : 객체가 생성될 때, 호출되는 함수
-	// 디폴트 생성자를 컴파일러가 만들어준다
-	// Student(){}
+	Student(void)
+	{
+		nHakbun = 1234;
+		sName = "이사랑";
+		cout << "학번이 등록되었습니다." << endl;
+	}
 };
 
 
