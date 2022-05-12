373ff36b42e93a2ab4020578fd7e4ad768b74b61
MiJi <MiJi@School>
2022-05-12 11:03:12 +0900

    배열 동적할당

--- 202/C++기본/03-02 동적할당.cpp
+++ 202/C++기본/03-02 동적할당.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main(void)
 {
-	int* ptr1 = new int;	// int형 공간 동적할당
-	*ptr1 = 20;
-	cout << *ptr1 << endl;
+	int* ptr2 = new int[4];
+	for (int i = 0; i < 4; i++)
+	{
+		ptr2[i] = 10 + i;
+		cout << ptr2[i] << " ";
+	}
+	delete []ptr2;
 
-	delete ptr1;
+	return 0;
 }
