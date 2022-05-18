5580be4a29d559b12ffaa7b90a4735bc7e07531b
MiJi <MiJi@School>
2022-05-18 09:01:44 +0900

    string을 char*형식으로 전환

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -7,12 +7,12 @@ class Student
 {
 private:
 	int nHakbun;
-	string sName;
+	char* sName;
 
 public:
 	// 생성자 : 객체가 생성될 때, 호출되는 함수
 	Student(void);
-	Student(int Hakbun, string Name);
+	Student(int Hakbun, const char* Name);
 
 	void show(void);
 };
@@ -20,11 +20,8 @@ public:
 
 int main(void)
 {
-	Student* stu = new Student[6];
-	for (int i = 0; i < 6; i++)
-		stu[i].show();
-
-	delete []stu;
+	Student stu1 = Student(1111, "JWP");
+	stu1.show();
 
 	return 0;
 }
@@ -32,17 +29,17 @@ int main(void)
 
 Student::Student(void)
 {
-	this->nHakbun = 1234;
-	this->sName = "이사랑";
-	cout << "학번이 등록되었습니다." << endl;
 }
 
-Student::Student(int Hakbun, string Name)
+Student::Student(int Hakbun, const char* Name)
 	// 객체생성과 동시에 멤버변수 초기화 // 멤버변수(매개변수)
 	// const/참조형 멤버변수를 사용할 수 있다.
-	: nHakbun(Hakbun), sName(Name)
+	: nHakbun(Hakbun)
 {
-	cout << "학번이 등록되었습니다." << endl;
+	cout << "일반생성자 호출" << endl;
+	int len = strlen(Name) + 1;		// 공간개수 측정
+	sName = new char[len];			// 공간생성
+	strcpy(sName, Name);
 }
 
 void Student::show(void)
