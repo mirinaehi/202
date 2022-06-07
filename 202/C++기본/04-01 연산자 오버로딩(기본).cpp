de744821595be412c6157b8a2203f064a22076f3
MiJi <MiJi@School>
2022-06-08 08:56:17 +0900

    파일분할

--- 202/C++기본/04-01 연산자 오버로딩(기본).cpp
+++ 202/C++기본/04-01 연산자 오버로딩(기본).cpp
@@ -1,31 +1,7 @@
 #include <iostream> // cout, endl 객체를 가지고 있음
+#include "04-01.h"
 
 using namespace std;
-
-class Point
-{
-public:
-	// 생성자
-	Point(int _x, int _y);
-	Point(void);
-
-	// 연산자 오버로딩
-	Point operator+(const Point& rhs)
-	{
-		Point p;
-		p.mX = mX + rhs.mX;
-		p.mY = mY + rhs.mY;
-
-		return p;
-	}
-	
-	void print(void);
-	
-
-private:
-	int mX;
-	int mY;
-};
 
 int main(void)
 {
@@ -36,13 +12,5 @@ int main(void)
 	sum.print();		// (4, 6)
 
 	return 0;
-}
-
-Point::Point(int _x, int _y) : mX(_x), mY(_y) {}
-Point::Point(void) {}
-
-void Point::print(void)
-{
-	cout << "(" << mX << ", " << mY << ")" << endl;
 }
 
